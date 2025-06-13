c267d4c11c33ad8e15b58a7744127eb2506b5f65
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2025-06-13 08:29:47 +0200

    main - route_name bevezetés

--- database/seeders/MenuItemSeeder.php
+++ database/seeders/MenuItemSeeder.php
@@ -20,63 +20,174 @@ class MenuItemSeeder extends Seeder
 
         // Főmenü - Home
         $home = MenuItem::create(
-            [ 'label' => 'home', 'url' => '/home', 'default_weight' => 1, ]
+            [
+                'label' => 'home',
+                //'url' => '/home',
+                'route_name' => 'dashboard',
+                'default_weight' => 1,
+            ]
         );
 
         $home->children()->create(
-            [ 'label' => 'dashboard', 'url' => '/dashboard', 'default_weight' => 1, ]
+            [
+                'label' => 'dashboard',
+                'url' => '/dashboard',
+                'route_name' => '',
+                'default_weight' => 1,
+            ]
         );
 
         // Főmenü - Administration
         $administration = MenuItem::create(
-            [ 'label' => 'administration', 'url' => null, 'default_weight' => 2, ]
+            [
+            'label' => 'administration',
+            'url' => null,
+            'route_name' => null,
+            'default_weight' => 2,
+        ]
         );
 
         $security = $administration->children()->create(
-            ['label' => 'security', 'url' => null, 'default_weight' => 1,]
+            [
+            'label' => 'security',
+            'url' => null,
+            'route_name' => null,
+            'default_weight' => 1,
+        ]
         );
 
         $security->children()->createMany([
-                [ 'label' => 'users', 'url' => '/users', 'default_weight' => 1, ],
-                [ 'label' => 'roles', 'url' => '/roles', 'default_weight' => 2, ],
-                [ 'label' => 'permissions', 'url' => '/permissions', 'default_weight' => 3, ],
-                [ 'label' => 'activities', 'url' => '/activities', 'default_weight' => 4, ]
+                [
+                    'label' => 'users',
+                    'url' => '/users',
+                    'route_name' => '',
+                    'default_weight' => 1,
+                ],
+                [
+                    'label' => 'roles',
+                    'url' => '/roles',
+                    'route_name' => '',
+                    'default_weight' => 2,
+                ],
+                [
+                    'label' => 'permissions',
+                    'url' => '/permissions',
+                    'route_name' => '',
+                    'default_weight' => 3,
+                ],
+                [
+                    'label' => 'activities',
+                    'url' => '/activities',
+                    'route_name' => '',
+                    'default_weight' => 4,
+                    ]
             ]);
 
         // Főmenü - System
         $system = MenuItem::create(
-            [ 'label' => 'system', 'url' => null, 'default_weight' => 3, ]
+            [
+                'label' => 'system',
+                'url' => null,
+                'route_name' => null,
+                'default_weight' => 3,
+            ]
         );
 
         $geo = $system->children()->create(
-            [ 'label' => 'geo', 'url' => null, 'default_weight' => 1, ]
+            [
+                'label' => 'geo',
+                'url' => null,
+                'route_name' => null,
+                'default_weight' => 1,
+            ]
         );
 
         $geo->children()->createMany(
             [
-                [ 'label' => 'countries', 'url' => '/countries', 'default_weight' => 1, ],
-                [ 'label' => 'regions', 'url' => '/regions', 'default_weight' => 2, ],
-                [ 'label' => 'cities', 'url' => '/cities', 'default_weight' => 3, ],
+                [
+                    'label' => 'countries',
+                    'url' => '/countries',
+                    'route_name' => '',
+                    'default_weight' => 1,
+                ],
+                [
+                    'label' => 'regions',
+                    'url' => '/regions',
+                    'route_name' => '',
+                    'default_weight' => 2,
+                ],
+                [
+                    'label' => 'cities',
+                    'url' => '/cities',
+                    'route_name' => '',
+                    'default_weight' => 3,
+                ],
             ]
         );
 
         $system->children()->createMany([
-            [ 'label' => 'subdomain_states', 'url' => '/subdomain_states', 'default_weight' => 2, ],
-            [ 'label' => 'acs_systems', 'url' => '/acs_systems', 'default_weight' => 3, ],
-            [ 'label' => 'app_settings', 'url' => '/app_settings', 'default_weight' => 4, ],
-            [ 'label' => 'comp_settings', 'url' => '/comp_settings', 'default_weight' => 5, ],
+            [
+                'label' => 'subdomain_states',
+                'url' => '/subdomain_states',
+                'route_name' => '',
+                'default_weight' => 2,
+            ],
+            [
+                'label' => 'acs_systems',
+                'url' => '/acs_systems',
+                'route_name' => '',
+                'default_weight' => 3,
+            ],
+            [
+                'label' => 'app_settings',
+                'url' => '/app_settings',
+                'route_name' => '',
+                'default_weight' => 4,
+            ],
+            [
+                'label' => 'comp_settings',
+                'url' => '/comp_settings',
+                'route_name' => '',
+                'default_weight' => 5,
+            ],
         ]);
 
         // Főmenü - Specimens
         $specimens = MenuItem::create(
-            [ 'label' => 'specimens', 'url' => null, 'default_weight' => 4, ]
+            [
+                'label' => 'specimens',
+                'url' => null,
+                'route_name' => null,
+                'default_weight' => 4,
+            ]
         );
 
         $specimens->children()->createMany(
             [
-                [ 'label' => 'companies', 'url' => '/companies', 'default_weight' => 1, ],
-                [ 'label' => 'subdomains', 'url' => '/subdomains', 'default_weight' => 2, ],
-                [ 'label' => 'entities', 'url' => '/entities', 'default_weight' => 3, ],
+                [
+                    'label' => 'companies',
+                    'url' => '/companies',
+                    'route_name' => '',
+                    'default_weight' => 1,
+                ],
+                [
+                    'label' => 'subdomains',
+                    'url' => '/subdomains',
+                    'route_name' => '',
+                    'default_weight' => 2,
+                ],
+                [
+                    'label' => 'entities',
+                    'url' => '/entities',
+                    'route_name' => '',
+                    'default_weight' => 3,
+                ],
+                [
+                    'label' => 'employees',
+                    //'url' => '/employees',
+                    'route_name' => 'employees.index',
+                    'default_weight' => 4,
+                ],
             ]
         );
     }
