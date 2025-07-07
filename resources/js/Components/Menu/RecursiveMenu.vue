c7dc464b15591d470dce35563b244b69679555c8
KovacsZoltan65 <zoltan1_kovacs@msn.com>
2025-07-07 09:28:23 +0200

    new_menu - mentés

---
+++ resources/js/Components/Menu/RecursiveMenu.vue
@@ -0,0 +1,87 @@
+<script setup>
+import { ref } from 'vue'
+import RecursiveMenuItem from '@/Components/Menu/RecursiveMenuItem.vue';
+
+const menu = ref([
+  {
+    items: [
+      {
+        label: 'home',
+        icon: 'pi pi-home',
+        to: null,
+        items: [
+          {
+            label: 'dashboard',
+            icon: 'pi pi-th-large',
+            to: 'http://company-02.mt/dashboard',
+            items: []
+          }
+        ]
+      },
+      {
+        label: 'administration',
+        icon: 'pi pi-cog',
+        to: null,
+        items: [
+          {
+            label: 'employees',
+            icon: 'pi pi-users',
+            to: 'http://company-02.mt/employees',
+            items: []
+          },
+          {
+            label: 'hierarchy',
+            icon: 'pi pi-share-alt',
+            to: 'http://company-02.mt/hierarchy',
+            items: []
+          },
+          {
+            label: 'companies',
+            icon: 'pi pi-building',
+            to: 'http://company-02.mt/companies',
+            items: []
+          }
+        ]
+      },
+      {
+        label: 'reports',
+        icon: 'pi pi-chart-line',
+        to: null,
+        items: [
+          {
+            label: 'monthly',
+            icon: 'pi pi-calendar',
+            to: null,
+            items: [
+              {
+                label: 'monthly_01',
+                icon: '',
+                to: 'http://company-02.mt/dashboard',
+                items: []
+              }
+            ]
+          },
+          {
+            label: 'annual',
+            icon: 'pi pi-file',
+            to: 'http://company-02.mt/dashboard',
+            items: []
+          }
+        ]
+      }
+    ]
+  }
+])
+</script>
+
+<template>
+  <ul class="space-y-2 p-4 bg-white rounded-md text-sm">
+    <template v-for="(group, g) in menu" :key="g">
+      <RecursiveMenuItem
+        v-for="(item, i) in group.items"
+        :key="i"
+        :item="item"
+      />
+    </template>
+  </ul>
+</template>
---
+++ resources/js/Components/Menu/RecursiveMenuItem.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  item: Object
+})
+</script>
+
+<template>
+  <li>
+    <div class="flex items-center gap-2 font-medium py-1">
+      <i :class="item.icon" />
+      <a
+        v-if="item.to"
+        :href="item.to"
+        class="hover:underline text-blue-600"
+      >{{ item.label }}</a>
+      <span v-else class="cursor-default">{{ item.label }}</span>
+    </div>
+
+    <ul
+      v-if="item.items && item.items.length"
+      class="ml-4 pl-2 border-l border-gray-300"
+    >
+      <RecursiveMenuItem
+        v-for="(child, i) in item.items"
+        :key="i"
+        :item="child"
+      />
+    </ul>
+  </li>
+</template>
